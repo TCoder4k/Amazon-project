f3e6fe71a3df920e72aca71680f5d9d0d7e98084
TCoder4k <tucaqn1@gmail.com>
2026-04-03 19:54:00 +0700

    save data and generate html for delivery option

--- data/cart.js
+++ data/cart.js
@@ -3,11 +3,14 @@ if(!cart){
   cart = [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 3
+    quantity: 3,
+    deliveryOptionId: '1'
+
   },
   {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '2'
   }];
 }
 export function updateQuantity(productId, newQuantity){
@@ -42,6 +45,7 @@ export function addToCart(productId, quantity) {
     cart.push({
       productId,
       quantity,
+      deliveryOptionId:'1'
     });
   }
   saveToStorage();
